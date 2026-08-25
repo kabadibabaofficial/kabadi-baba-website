ed064a6ff4a769c1d97f9d8433f50acc0d3f9020
kabadibabaofficial <kabadibabaofficial@gmail.com>
2026-08-25 11:44:53 +0530

    Add Google Analytics tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Script from "next/script";
 
 const siteUrl = "https://www.kabadibaba.com";
 
@@ -53,6 +54,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="min-h-full flex flex-col">
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-51MHTVV0K7"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag("js", new Date());
+            gtag("config", "G-51MHTVV0K7");
+          `}
+        </Script>
         {children}
       </body>
     </html>
